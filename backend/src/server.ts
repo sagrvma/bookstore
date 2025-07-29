46b5ab3b1ec2498f5c0924d12ee511082bc2d2bb
sagrvma <notsagarverma@gmail.com>
2025-07-29 21:52:31 +0530

    connected author routes to server, works just right

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,20 @@
 import dotenv from "dotenv";
+import express from "express";
 import connectToDB from "./config/database";
+import authorRouter from "./routes/authorRoutes";
 
 dotenv.config();
 
 connectToDB();
+
+const app = express();
+
+app.use(express.json()); //JSON Middleware
+
+app.use("/api/authors", authorRouter);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is now successfully running on port ${PORT}`);
+});
