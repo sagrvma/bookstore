17a95b3c0c458289a482b1b4e69ae666cdb3f8ea
sagrvma <notsagarverma@gmail.com>
2025-08-20 22:43:00 +0530

    added authController logic for registering a new user

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-import { IUser } from "../models/user";
+import User, { IUser } from "../models/user";
 import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
 
-const genJWT = (user: IUser) => {
+const genJWT = (user: IUser): string => {
   const secret = process.env.JWT_SECRET;
   if (!secret) {
     throw new Error("JWT secret env variable is not set.");
@@ -10,7 +11,7 @@ const genJWT = (user: IUser) => {
   const accessToken = jwt.sign(
     {
       //Bearer token
-      userId: user._id,
+      userId: (user._id as mongoose.Types.ObjectId).toString(), //Avoiding TS error in IDE
       username: user.name,
       role: user.role,
     },
@@ -21,4 +22,57 @@ const genJWT = (user: IUser) => {
   );
 
   return accessToken;
+};
+
+const register = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    //Check if user already exists
+    const existingUser: IUser | null = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists!",
+      });
+    }
+
+    //Create new user
+    const newUser: IUser = new User({ name, email, password, role }); //Password will be automatically hashed by our middleware in user
+    await newUser.save();
+
+    //Generate JWT Token
+    const token: string = genJWT(newUser);
+
+    return res.status(201).json({
+      success: true,
+      message: "New user registered successfully.",
+      data: {
+        user: {
+          id: newUser._id,
+          name: newUser.name,
+          email: newUser.email,
+          role: newUser.role,
+        },
+        token,
+      },
+    });
+  } catch (error: any) {
+    console.error("Error while trying to register user: ", error);
+    if (error.name === "ValidationError") {
+      return res.status(400).json({
+        success: false,
+        message: "Validation Error",
+        errors: error.errors || error.message || "Unknown error!",
+      });
+    }
+
+    return res.status(500).json({
+      success: false,
+      message:
+        "Something went wrong while registering new user! Please try again.",
+      errors: error.errors || error.message || "Unknown error!",
+    });
+  }
 };
