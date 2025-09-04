a576cd6864e4c156173f0a043516cae7dc698efe
sagrvma <notsagarverma@gmail.com>
2025-09-04 22:24:33 +0530

    added protect, restrictTo middleware to book and author routes

--- backend/src/routes/authorRoutes.ts
+++ backend/src/routes/authorRoutes.ts
@@ -6,18 +6,22 @@ import {
   getAuthors,
   updateAuthor,
 } from "../controllers/authorController";
+import { protect, restrictTo } from "../middleware/authMiddleware";
 
 const router = Router();
 
+//PUBLIC ROUTES
 //GET - /api/authors - Get all authors
 router.get("/", getAuthors);
 //GET - /api/authors/:id - Get author by id or slug
 router.get("/:id", getAuthorById);
+
+//PROTECTED ROUTES
 //POST - /api/authors - Create a new author
-router.post("/", createAuthor);
+router.post("/", protect, restrictTo("admin"), createAuthor);
 //DELETE - /api/authors/:id - Remove author by id or slug
-router.delete("/:id", deleteAuthor);
+router.delete("/:id", protect, restrictTo("admin"), deleteAuthor);
 //PATCH - api/authors/:id - Update author by id or slug
-router.patch("/:id", updateAuthor);
+router.patch("/:id", protect, restrictTo("admin"), updateAuthor);
 
 export default router;
--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -6,18 +6,21 @@ import {
   removeBook,
   updateBook,
 } from "../controllers/bookController";
-
+import { protect, restrictTo } from "../middleware/authMiddleware";
 const router = Router();
 
+//PUBLIC ROUTES
 //GET - /api/books - Get all books
 router.get("/", getBooks);
 //GET - /api/books/:id - Get book by id
 router.get("/:id", getBookById);
+
+//PROTECTED ROUTES
 //POST - /api/books - Create a new book
-router.post("/", createBook);
+router.post("/", protect, restrictTo("admin"), createBook);
 //DELETE - /api/books/:id - Delete book by id
-router.delete("/:id", removeBook);
+router.delete("/:id", protect, restrictTo("admin"), removeBook);
 //PATCH - /api/books/:id - Update book by id
-router.patch("/:id", updateBook);
+router.patch("/:id", protect, restrictTo("admin"), updateBook);
 
 export default router;
