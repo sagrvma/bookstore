71177acab80a428926a3ed51e692df664e2c9c55
sagrvma <notsagarverma@gmail.com>
2025-09-09 22:20:18 +0530

    order related schema and interfaces minor fixes

--- backend/src/models/order.ts
+++ backend/src/models/order.ts
@@ -16,7 +16,7 @@ export interface IShippingAddress {
   street: string;
   city: string;
   state: string;
-  zipCode: string;
+  pinCode: string;
   country: string;
   phone: string;
 }
@@ -26,13 +26,13 @@ export interface IOrder extends Document {
   orderNumber: string; //Human friendly ID (ORD-78654321-001)
   items: mongoose.Types.DocumentArray<IOrderItem & mongoose.Types.Subdocument>; //For TS, basically is just IOrderItem[]
   subtotal: number; //Sum of lineTotals of all order items
-  tax: number; // Tax Amount (0 for now)
-  shippingCost: number; // Shipping cost (0 for now)
+  tax?: number; // Tax Amount (0 for now)
+  shippingCost?: number; // Shipping cost (0 for now)
   totalAmount: number; //subtotal+tax+totalAmount
-  status: "pending" | "confirmed" | "shipped" | "delivered" | "cancelled";
-  paymentStatus: "pending" | "completed" | "failed" | "refunded";
-  paymentMethod: "cash_on_delivery" | "card";
-  paymentIntentID?: string; //Stripe Payment intent ID
+  status?: "pending" | "confirmed" | "shipped" | "delivered" | "cancelled";
+  paymentStatus?: "pending" | "completed" | "failed" | "refunded";
+  paymentMethod?: "cash_on_delivery" | "card";
+  paymentIntentId?: string; //Stripe Payment intent ID
   shippingAddress: IShippingAddress;
   notes?: string; //Order notes, extra info if available/needed
   createdAt: Date;
@@ -78,47 +78,47 @@ const shippingAddressSchema = new Schema(
       type: String,
       required: [true, "Full name is required."],
       trim: true,
-      max: [0, "Full name can't exceed 100 characters."],
+      maxlength: [100, "Full name can't exceed 100 characters."],
     },
     street: {
       type: String,
       required: [true, "Street address is required."],
       trim: true,
-      max: [200, "Street address can't exceed 200 characters."],
+      maxlength: [200, "Street address can't exceed 200 characters."],
     },
     city: {
       type: String,
       required: [true, "City is required."],
       trim: true,
-      max: [50, "City can't exceed 50 characters."],
+      maxlength: [50, "City can't exceed 50 characters."],
     },
     state: {
       type: String,
       required: [true, "State is required."],
       trim: true,
-      max: [50, "State can't exceed 50 characters."],
+      maxlength: [50, "State can't exceed 50 characters."],
     },
-    zipCode: {
+    pinCode: {
       type: String,
-      required: [true, "Zip Code is required."],
+      required: [true, "Pin Code is required."],
       trim: true,
-      match: [/^d{6}$/, "Please enter a valid Zip Code."],
+      match: [/^[1-9][0-9]{5}$/, "Please enter a valid Pin Code."],
     },
     country: {
       type: String,
       required: [true, "Country is required."],
       trim: true,
-      max: [50, "Country can't exceed 50 characters."],
+      maxlength: [50, "Country can't exceed 50 characters."],
     },
     phone: {
-      type: Number,
+      type: String,
       required: [true, "Phone number is required."],
       trim: true,
       match: [/^\+?[\d\s-()]{10,15}$/, "Please enter a valid phone number."],
     },
   },
   {
-    id: false, // ID not needed for shipping address.
+    _id: false, // ID not needed for shipping address.
   }
 );
 
@@ -138,6 +138,13 @@ const orderSchema = new Schema(
 
     items: {
       type: [orderItemSchema],
+      required: [true, "Order items are required."],
+      validate: {
+        validator: (items: Array<IOrderItem>) => {
+          return items && items.length > 0;
+        },
+        message: "Order must have atleast 1 item.",
+      },
     },
     subtotal: {
       type: Number,
@@ -146,13 +153,11 @@ const orderSchema = new Schema(
     },
     tax: {
       type: Number,
-      required: [true, "Tax is required."],
       default: 0,
       min: [0, "Tax can't be negative."],
     },
     shippingCost: {
       type: Number,
-      required: [true, "Shipping cost is required."],
       default: 0,
       min: [0, "Shipping cost can't be negative."],
     },
@@ -164,6 +169,7 @@ const orderSchema = new Schema(
     status: {
       type: String,
       enum: ["pending", "confirmed", "shipped", "delivered", "cancelled"],
+      default: "pending",
     },
     paymentStatus: {
       type: String,
@@ -172,7 +178,6 @@ const orderSchema = new Schema(
     },
     paymentMethod: {
       type: String,
-      required: [true, "Payment method is required."],
       enum: ["cash_on_delivery", "card"],
       default: "cash_on_delivery",
     },
@@ -202,12 +207,12 @@ orderSchema.pre("save", async function (next) {
     const count = await mongoose.model("Order").countDocuments({
       createdAt: {
         $gte: new Date(new Date().setHours(0, 0, 0, 0)),
-        $lte: new Date(new Date().setHours(23, 59, 59, 59)),
+        $lt: new Date(new Date().setHours(23, 59, 59, 59)),
       },
     });
     this.orderNumber = `ORD-${date}-${String(count + 1).padStart(3, "0")}`; //Pad start makes sure it is a 3 digit with as 0 prefixes as required, 1 becomes 001, 12 becomes 012 etc.
-    next();
   }
+  next();
 });
 
 //Indexes for efficient queryies
