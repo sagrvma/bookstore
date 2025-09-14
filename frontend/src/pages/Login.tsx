1577fa16a263359fe936dceada224427dac74c28
sagrvma <notsagarverma@gmail.com>
2025-09-14 18:44:40 +0530

    added basic Login component to teest auth api integration

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import http, { tokenStore } from "../lib/http";
+
+type AuthPayload = {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    role: "user" | "admin";
+  };
+  token: string;
+};
+
+type APISuccess<T> = {
+  success: true;
+  message: string;
+  data: T;
+};
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [err, setErr] = useState("");
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setErr("");
+    try {
+      const res = await http.post<APISuccess<AuthPayload>>("/api/auth/login", {
+        email,
+        password,
+      });
+      const token = res.data.data.token;
+
+      tokenStore.set(token);
+
+      navigate("/books", { replace: true });
+    } catch (error: any) {
+      const msg =
+        error?.response?.data?.message ||
+        error?.message ||
+        "Login failed! Please try again.";
+      setErr(msg);
+    }
+  };
+
+  return (
+    <form className="loginForm" onSubmit={onSubmit}>
+      <label>
+        Email
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email address"
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+      </label>
+      <button type="submit">Sign In</button>
+      {err && <p className="error">{err}</p>}
+    </form>
+  );
+};
