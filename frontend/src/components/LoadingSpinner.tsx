6d3927856a9c01a26f33263b02c8f13bc2aa142e
sagrvma <notsagarverma@gmail.com>
2026-02-10 09:04:01 +0530

    added loading spinner component

---
+++ frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import styles from "./LoadingSpinner.module.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className={styles.loadingWrapper}>
+      <div className={styles.spinner}></div>
+      <p className={styles.loadingText}>Loading...</p>
+    </div>
+  );
+};
