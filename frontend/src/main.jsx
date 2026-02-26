bfceb16362ca3daedd4e5385dd7c0a1554a9191b
sagrvma <notsagarverma@gmail.com>
2026-02-26 08:58:44 +0530

    wrapped main.jsx in ThemeProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,114 +23,117 @@ import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import AdminRoute from "./routes/AdminRoute.tsx";
 import { ToastProvider } from "./context/ToastContext.tsx";
 import ToastContainer from "./components/Toast.tsx";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToastProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<App />} />
-          {/*Login page*/}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {/*Public Books page*/}
-          <Route path="/books" element={<Books />} />
-          {/*Protected Routes*/}
-          <Route
-            path="/cart"
-            element={
-              <ProtectedRoute>
-                <CartPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/checkout"
-            element={
-              <ProtectedRoute>
-                <Checkout />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/orders"
-            element={
-              <ProtectedRoute>
-                <Orders />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/orders/:orderId"
-            element={
-              <ProtectedRoute>
-                <OrderDetails />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/orders"
-            element={
-              <AdminRoute>
-                <AdminOrders />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/books"
-            element={
-              <AdminRoute>
-                <AdminBooks />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/authors"
-            element={
-              <AdminRoute>
-                <AdminAuthors />
-              </AdminRoute>
-            }
-          />
+    <ThemeProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<App />} />
+            {/*Login page*/}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/*Public Books page*/}
+            <Route path="/books" element={<Books />} />
+            {/*Protected Routes*/}
+            <Route
+              path="/cart"
+              element={
+                <ProtectedRoute>
+                  <CartPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/checkout"
+              element={
+                <ProtectedRoute>
+                  <Checkout />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/orders"
+              element={
+                <ProtectedRoute>
+                  <Orders />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/orders/:orderId"
+              element={
+                <ProtectedRoute>
+                  <OrderDetails />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/orders"
+              element={
+                <AdminRoute>
+                  <AdminOrders />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/books"
+              element={
+                <AdminRoute>
+                  <AdminBooks />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/authors"
+              element={
+                <AdminRoute>
+                  <AdminAuthors />
+                </AdminRoute>
+              }
+            />
 
-          <Route path="/books/:bookId" element={<BookDetails />} />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile/edit"
-            element={
-              <ProtectedRoute>
-                <EditProfile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile/password"
-            element={
-              <ProtectedRoute>
-                <ChangePassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile/addresses"
-            element={
-              <ProtectedRoute>
-                <ManageAddresses />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+            <Route path="/books/:bookId" element={<BookDetails />} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile/edit"
+              element={
+                <ProtectedRoute>
+                  <EditProfile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile/password"
+              element={
+                <ProtectedRoute>
+                  <ChangePassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile/addresses"
+              element={
+                <ProtectedRoute>
+                  <ManageAddresses />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
 
-        <ToastContainer />
-      </BrowserRouter>
-    </ToastProvider>
-  </StrictMode>
+          <ToastContainer />
+        </BrowserRouter>
+      </ToastProvider>
+    </ThemeProvider>
+  </StrictMode>,
 );
